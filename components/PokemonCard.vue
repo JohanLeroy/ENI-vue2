8f00c83b08e95c1487295e184e7d0ee9cbeb2c5c
jleroy2023 <leroyjohan3@gmail.com>
2025-07-15 13:24:23 +0200

    FIX

--- components/PokemonCard.vue
+++ components/PokemonCard.vue
@@ -1,7 +1,7 @@
 <template>
   <v-col cols="12" sm="4" md="3">
     <v-card class="hover:scale-105 transition" @click="$router.push('/pokemon/' + pokemon.pokedex_id)">
-      <v-img :src="pokemon.sprites.regular" height="200px" />
+      <v-img :src="pokemon.sprites.regular" height="200px h-20" />
       <v-card-title>{{ pokemon.name.fr }}</v-card-title>
     </v-card>
   </v-col>
